34d01cb3c75ffb08499dc16623c8a4c82c0b6b2f
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-02-08 11:03:22 +0100

    quality PoC

--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
-	"log"
 	"os"
-	"regexp"
-	"runtime"
-	"runtime/pprof"
+	"strings"
 	"time"
-
-	"github.com/paulcacheux/xmlite/xml"
 )
 
 const TEST_PATH = "./testdata/f48bc264f9ca35fa6d482a6ffb71ba5379093364-primary.xml"
-const CPU_PPROF_PATH = "./cpu.pprof"
-const MEM_PPROF_PATH = "./mem.pprof"
 
 func main() {
 	f, err := os.Open(TEST_PATH)
@@ -26,60 +18,126 @@ func main() {
 	}
 	defer f.Close()
 
-	// CPU profiling
-	prof, err := os.Create(CPU_PPROF_PATH)
-	if err != nil {
-		log.Fatal("could not create CPU profile: ", err)
-	}
-	defer prof.Close()
-	if err := pprof.StartCPUProfile(prof); err != nil {
-		log.Fatal("could not start CPU profile: ", err)
-	}
-	defer pprof.StopCPUProfile()
-
-	freader := &FilterReader{inner: f}
-	reader := bufio.NewReaderSize(freader, 1024)
-
-	decoder := xml.NewDecoder(reader)
+	decoder := NewLiteDecoder(f)
+	names := make(map[string]bool)
 
 	start := time.Now()
-
 	for {
-		_, err := decoder.RawToken()
+		name, err := decoder.Next()
 		if err != nil {
 			fmt.Println(err)
 			break
 		}
+		if name == "" {
+			panic("WTF")
+		}
+		names[name] = true
 	}
 
 	fmt.Printf("elapsed: %v\n", time.Since(start))
+	fmt.Println(names)
+}
 
-	// Heap profiling
-	mem, err := os.Create(MEM_PPROF_PATH)
-	if err != nil {
-		log.Fatal("could not create memory profile: ", err)
-	}
-	defer mem.Close()
-	runtime.GC()
-	if err := pprof.WriteHeapProfile(mem); err != nil {
-		panic(fmt.Errorf("could not write memory profile: %w", err))
+type LiteDecoder struct {
+	reader    *bufio.Reader
+	peekStore int
+}
+
+func NewLiteDecoder(reader io.Reader) *LiteDecoder {
+	return &LiteDecoder{
+		reader:    bufio.NewReader(reader),
+		peekStore: -1,
 	}
 }
 
-type FilterReader struct {
-	inner io.Reader
-}
-
-var re = regexp.MustCompile(`<rpm:entry\s+name="[^"]*?\([^"]*?".*?\/>`)
-
-func (fr *FilterReader) Read(p []byte) (int, error) {
-	n, err := fr.inner.Read(p)
-	if err != nil || !bytes.Contains(p, []byte("<rpm:entry")) {
-		return n, err
+func (lt *LiteDecoder) getc() (byte, error) {
+	if lt.peekStore >= 0 {
+		val := lt.peekStore
+		lt.peekStore = -1
+		return byte(val), nil
 	}
 
-	fixedBuf := re.ReplaceAllLiteral(p, nil)
-	resCount := len(fixedBuf)
-	copy(p[:resCount], fixedBuf)
-	return resCount, nil
+	return lt.reader.ReadByte()
+}
+
+func (lt *LiteDecoder) peekc() (byte, error) {
+	if lt.peekStore < 0 {
+		b, err := lt.reader.ReadByte()
+		if err != nil {
+			return b, err
+		}
+		lt.peekStore = int(b)
+	}
+	return byte(lt.peekStore), nil
+}
+
+func (lt *LiteDecoder) clearPeek() {
+	lt.peekStore = -1
+}
+
+func (lt *LiteDecoder) Next() (string, error) {
+	for {
+		curr, err := lt.getc()
+		if err != nil {
+			return "", err
+		}
+
+		if curr != '<' {
+			continue
+		}
+
+		curr1, err := lt.peekc()
+		if err != nil {
+			return "", err
+		}
+
+		if curr1 == '/' {
+			lt.clearPeek()
+		} else if curr1 == '?' {
+			lt.skipUntil('>')
+			continue
+		}
+
+		name, err := lt.name()
+		if err != nil {
+			return "", err
+		}
+
+		lt.skipUntil('>')
+		return name, nil
+	}
+}
+
+func (lt *LiteDecoder) skipUntil(target byte) error {
+	for {
+		curr, err := lt.getc()
+		if err != nil {
+			return err
+		}
+
+		if curr == target {
+			return nil
+		}
+	}
+}
+
+func (lt *LiteDecoder) name() (string, error) {
+	var buff strings.Builder
+	for {
+		curr, err := lt.peekc()
+		if err != nil {
+			return "", err
+		}
+
+		if isNameChar(curr) {
+			lt.clearPeek()
+			buff.WriteByte(curr)
+		} else {
+			return buff.String(), nil
+		}
+	}
+}
+
+func isNameChar(c byte) bool {
+	return c == ':' || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
 }
